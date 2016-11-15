c64b426848913d7a54b4b2043d1e9628f47d0af1
Lauri Raivio <lauri.raivio@gmail.com>
2016-11-15 19:29:21 +0200

    Add api utils
    Refs #3.

---
+++ app/api/utils.js
@@ -0,0 +1,114 @@
+import { camelizeKeys, decamelizeKeys } from 'humps';
+import pickBy from 'lodash/pickBy';
+import isEmpty from 'lodash/isEmpty';
+import { normalize } from 'normalizr';
+import { CALL_API, getJSON } from 'redux-api-middleware';
+
+const requiredHeaders = {
+  Accept: 'application/json',
+  'Accept-Language': 'fi',
+  'Content-Type': 'application/json',
+};
+
+function buildAPIUrl(endpoint, params) {
+  let url = `${SETTINGS.API_URL}/${endpoint}/`;
+
+  const nonEmptyParams = pickBy(params, value => value !== '');
+
+  if (!isEmpty(nonEmptyParams)) {
+    url = `${url}?${getSearchParamsString(nonEmptyParams)}`;
+  }
+
+  return url;
+}
+
+function createTransformFunction(schema) {
+  return (json) => {
+    const camelizedJson = camelizeKeys(json);
+    if (schema) {
+      return normalize(camelizedJson, schema);
+    }
+    return camelizedJson;
+  };
+}
+
+function getErrorTypeDescriptor(type, options = {}) {
+  return {
+    type,
+    meta: action => ({
+      API_ACTION: {
+        apiRequestFinish: true,
+        countable: options.countable,
+        type: action[CALL_API].types[0].type,
+      },
+    }),
+  };
+}
+
+function getHeadersCreator(headers) {
+  return (state) => {
+    const authorizationHeaders = {};
+    if (state.auth && state.auth.token) {
+      authorizationHeaders.Authorization = `JWT ${state.auth.token}`;
+    }
+    return Object.assign({}, requiredHeaders, headers, authorizationHeaders);
+  };
+}
+
+function getRequestTypeDescriptor(type, options = {}) {
+  return {
+    type,
+    meta: Object.assign({
+      API_ACTION: {
+        apiRequestStart: true,
+        countable: options.countable,
+        type,
+      },
+    }, options.meta),
+  };
+}
+
+function getSearchParamsString(params) {
+  const decamelized = decamelizeKeys(params);
+  const parts = [];
+
+  Object.keys(decamelized).forEach((key) => {
+    parts.push(`${encodeURIComponent(key)}=${encodeURIComponent(decamelized[key])}`);
+  });
+
+  return parts.join('&');
+}
+
+function getSuccessPayload(options) {
+  return (action, state, response) => (
+    getJSON(response).then(createTransformFunction(options.schema))
+  );
+}
+
+function getSuccessTypeDescriptor(type, options = {}) {
+  return {
+    type,
+    payload: options.payload || getSuccessPayload(options),
+
+    meta: action => (
+      Object.assign({
+        API_ACTION: {
+          apiRequestFinish: true,
+          countable: options.countable,
+          type: action[CALL_API].types[0].type,
+        },
+      }, options.meta)
+    ),
+  };
+}
+
+export {
+  buildAPIUrl,
+  createTransformFunction,
+  getErrorTypeDescriptor,
+  getHeadersCreator,
+  getRequestTypeDescriptor,
+  getSearchParamsString,
+  getSuccessTypeDescriptor,
+  requiredHeaders,
+};
---
+++ app/api/utils.spec.js
@@ -0,0 +1,353 @@
+import { expect } from 'chai';
+import { CALL_API } from 'redux-api-middleware';
+
+import {
+  buildAPIUrl,
+  createTransformFunction,
+  getErrorTypeDescriptor,
+  getHeadersCreator,
+  getRequestTypeDescriptor,
+  getSearchParamsString,
+  getSuccessTypeDescriptor,
+  requiredHeaders,
+} from './utils';
+
+describe('api/utils', () => {
+  describe('buildAPIUrl', () => {
+    const endpoint = 'some/endpoint';
+
+    it('returns API_URL + given endpoint if params is empty', () => {
+      const expected = `${SETTINGS.API_URL}/${endpoint}/`;
+
+      expect(buildAPIUrl(endpoint)).to.equal(expected);
+    });
+
+    it('rejects params with empty values', () => {
+      const params = { empty: '' };
+      const expected = `${SETTINGS.API_URL}/${endpoint}/`;
+
+      expect(buildAPIUrl(endpoint, params)).to.equal(expected);
+    });
+
+    it('appends search params at the end if params is not empty', () => {
+      const params = { param: 'hello_world' };
+      const expected = `${SETTINGS.API_URL}/${endpoint}/?param=hello_world`;
+
+      expect(buildAPIUrl(endpoint, params)).to.equal(expected);
+    });
+  });
+
+  describe('createTransformFunction', () => {
+    it('returns a function', () => {
+      expect(typeof createTransformFunction()).to.equal('function');
+    });
+
+    describe('the returned function', () => {
+      it('camelizes object keys', () => {
+        const transformFunction = createTransformFunction();
+        const initial = {
+          some_key: {
+            nested_key: 'value',
+          },
+        };
+        const expected = {
+          someKey: {
+            nestedKey: 'value',
+          },
+        };
+
+        expect(transformFunction(initial)).to.deep.equal(expected);
+      });
+    });
+  });
+
+  describe('getErrorTypeDescriptor', () => {
+    const actionType = 'SOME_GET_ERROR';
+
+    it('returns an object', () => {
+      expect(typeof getErrorTypeDescriptor(actionType)).to.equal('object');
+    });
+
+    it('contains the given action type', () => {
+      const actual = getErrorTypeDescriptor(actionType).type;
+
+      expect(actual).to.equal(actionType);
+    });
+
+    it('contains a meta function', () => {
+      expect(typeof getErrorTypeDescriptor(actionType).meta).to.equal('function');
+    });
+
+    describe('the meta function', () => {
+      const mockAction = {
+        [CALL_API]: {
+          types: [{ type: 'SOME_GET_REQUEST' }],
+        },
+      };
+
+      it('returns an object with correct properties', () => {
+        const typeDescriptor = getErrorTypeDescriptor(actionType);
+        const actual = typeDescriptor.meta(mockAction);
+        const expected = {
+          API_ACTION: {
+            apiRequestFinish: true,
+            countable: undefined,
+            type: 'SOME_GET_REQUEST',
+          },
+        };
+
+        expect(actual).to.deep.equal(expected);
+      });
+
+      it('supports adding a countable property', () => {
+        const typeDescriptor = getErrorTypeDescriptor(actionType, { countable: true });
+        const actual = typeDescriptor.meta(mockAction);
+        const expected = {
+          API_ACTION: {
+            apiRequestFinish: true,
+            countable: true,
+            type: 'SOME_GET_REQUEST',
+          },
+        };
+
+        expect(actual).to.deep.equal(expected);
+      });
+    });
+  });
+
+  describe('getHeadersCreator', () => {
+    it('returns a function', () => {
+      expect(typeof getHeadersCreator()).to.equal('function');
+    });
+
+    describe('the returned function', () => {
+      describe('when user is logged in', () => {
+        const state = {
+          auth: {
+            token: 'mock-token',
+          },
+        };
+        const authorizationHeader = { Authorization: 'JWT mock-token' };
+
+        describe('if no additional headers are specified', () => {
+          it('returns the required headers and Authorization header', () => {
+            const creator = getHeadersCreator();
+            const expected = Object.assign(
+              {},
+              requiredHeaders,
+              authorizationHeader
+            );
+
+            expect(creator(state)).to.deep.equal(expected);
+          });
+        });
+
+        describe('if additional headers are specified', () => {
+          it('returns the required, the additional and Authorization headers', () => {
+            const additionalHeaders = {
+              header: 'value',
+            };
+            const creator = getHeadersCreator(additionalHeaders);
+            const expected = Object.assign(
+              {},
+              requiredHeaders,
+              additionalHeaders,
+              authorizationHeader
+            );
+
+            expect(creator(state)).to.deep.equal(expected);
+          });
+        });
+      });
+
+      describe('when user is logged out', () => {
+        const state = {
+          auth: {},
+        };
+
+        describe('if no additional headers are specified', () => {
+          it('returns the just the required headers', () => {
+            const creator = getHeadersCreator();
+
+            expect(creator(state)).to.deep.equal(requiredHeaders);
+          });
+        });
+
+        describe('if additional headers are specified', () => {
+          it('returns the required headers and the additional headers', () => {
+            const additionalHeaders = {
+              header: 'value',
+            };
+            const creator = getHeadersCreator(additionalHeaders);
+            const expected = Object.assign({}, requiredHeaders, additionalHeaders);
+
+            expect(creator(state)).to.deep.equal(expected);
+          });
+        });
+      });
+    });
+  });
+
+  describe('getRequestTypeDescriptor', () => {
+    const actionType = 'SOME_GET_REQUEST';
+
+    it('returns an object', () => {
+      expect(typeof getRequestTypeDescriptor(actionType)).to.equal('object');
+    });
+
+    it('contains the given action type', () => {
+      const actual = getRequestTypeDescriptor(actionType).type;
+
+      expect(actual).to.equal(actionType);
+    });
+
+    it('contains a meta object with correct properties', () => {
+      const actual = getRequestTypeDescriptor(actionType).meta;
+      const expected = {
+        API_ACTION: {
+          apiRequestStart: true,
+          countable: undefined,
+          type: 'SOME_GET_REQUEST',
+        },
+      };
+
+      expect(actual).to.deep.equal(expected);
+    });
+
+    it('supports adding coutable property to meta object', () => {
+      const actual = getRequestTypeDescriptor(actionType, { countable: true }).meta;
+      const expected = {
+        API_ACTION: {
+          apiRequestStart: true,
+          countable: true,
+          type: 'SOME_GET_REQUEST',
+        },
+      };
+
+      expect(actual).to.deep.equal(expected);
+    });
+
+    it('supports adding extra meta properties', () => {
+      const actual = getRequestTypeDescriptor(actionType, { meta: { test: 'test' } }).meta;
+      const expected = {
+        API_ACTION: {
+          apiRequestStart: true,
+          countable: undefined,
+          type: 'SOME_GET_REQUEST',
+        },
+        test: 'test',
+      };
+
+      expect(actual).to.deep.equal(expected);
+    });
+  });
+
+  describe('getSearchParamsString', () => {
+    it('returns key and value of the param with "=" in between', () => {
+      const params = { param: 'hello' };
+      const expected = 'param=hello';
+
+      expect(getSearchParamsString(params)).to.equal(expected);
+    });
+
+    it('returns multiple params separated with "&"', () => {
+      const params = { param: 'hello', other: 'world' };
+      const expected = 'param=hello&other=world';
+
+      expect(getSearchParamsString(params)).to.equal(expected);
+    });
+
+    it('uses encodeURIComponent to both keys and values', () => {
+      const params = { päräm: 'hellö' };
+      const expected = `${encodeURIComponent('päräm')}=${encodeURIComponent('hellö')}`;
+
+      expect(getSearchParamsString(params)).to.equal(expected);
+    });
+
+    it('decamelizes keys of the given params', () => {
+      const params = { camelizedParam: 'hello' };
+      const expected = 'camelized_param=hello';
+
+      expect(getSearchParamsString(params)).to.equal(expected);
+    });
+  });
+
+  describe('getSuccessTypeDescriptor', () => {
+    const actionType = 'SOME_GET_SUCCESS';
+
+    it('returns an object', () => {
+      expect(typeof getSuccessTypeDescriptor(actionType)).to.equal('object');
+    });
+
+    it('contains the given action type', () => {
+      const actual = getSuccessTypeDescriptor(actionType).type;
+
+      expect(actual).to.equal(actionType);
+    });
+
+    it('contains a payload function', () => {
+      expect(typeof getSuccessTypeDescriptor(actionType).payload).to.equal('function');
+    });
+
+    it('contains a meta function', () => {
+      expect(typeof getSuccessTypeDescriptor(actionType).meta).to.equal('function');
+    });
+
+    describe('the meta function', () => {
+      const mockAction = {
+        [CALL_API]: {
+          types: [{ type: 'SOME_GET_REQUEST' }],
+        },
+      };
+
+      it('returns an object with correct properties', () => {
+        const typeDescriptor = getSuccessTypeDescriptor(actionType);
+        const actual = typeDescriptor.meta(mockAction);
+        const expected = {
+          API_ACTION: {
+            apiRequestFinish: true,
+            countable: undefined,
+            type: 'SOME_GET_REQUEST',
+          },
+        };
+
+        expect(actual).to.deep.equal(expected);
+      });
+
+      it('supports adding a countable property', () => {
+        const typeDescriptor = getSuccessTypeDescriptor(actionType, { countable: true });
+        const actual = typeDescriptor.meta(mockAction);
+        const expected = {
+          API_ACTION: {
+            apiRequestFinish: true,
+            countable: true,
+            type: 'SOME_GET_REQUEST',
+          },
+        };
+
+        expect(actual).to.deep.equal(expected);
+      });
+
+      it('supports adding payload property', () => {
+        const typeDescriptor = getSuccessTypeDescriptor(actionType, { payload: 'mock-payload' });
+
+        expect(typeDescriptor.payload).to.equal('mock-payload');
+      });
+
+      it('supports adding extra meta properties', () => {
+        const typeDescriptor = getSuccessTypeDescriptor(actionType, { meta: { test: 'test' } });
+        const actual = typeDescriptor.meta(mockAction);
+        const expected = {
+          API_ACTION: {
+            apiRequestFinish: true,
+            countable: undefined,
+            type: 'SOME_GET_REQUEST',
+          },
+          test: 'test',
+        };
+
+        expect(actual).to.deep.equal(expected);
+      });
+    });
+  });
+});
