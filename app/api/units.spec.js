a0ea9f3576a521efd1d09169b06a4cd1e3981cbc
Lauri Raivio <lauri.raivio@gmail.com>
2016-11-16 08:03:09 +0200

    Add api unit actions
    Refs #3.

---
+++ app/api/units.spec.js
@@ -0,0 +1,27 @@
+import types from './actionTypes';
+import { createApiTest } from './testUtils';
+import { fetchUnits } from './units';
+import { buildAPIUrl } from './utils';
+
+describe('api/units', () => {
+  describe('fetchUnits', () => {
+    createApiTest({
+      name: 'fetchUnits',
+      action: fetchUnits,
+      args: [],
+      tests: {
+        method: 'GET',
+        endpoint: buildAPIUrl('unit', { pageSize: 100 }),
+        request: {
+          type: types.UNITS_GET_REQUEST,
+        },
+        success: {
+          type: types.UNITS_GET_SUCCESS,
+        },
+        error: {
+          type: types.UNITS_GET_ERROR,
+        },
+      },
+    });
+  });
+});
