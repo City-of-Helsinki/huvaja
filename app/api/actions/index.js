f3c5f19aae2c3b0fd83e29046d4c98fb90a56aab
Lauri Raivio <lauri.raivio@gmail.com>
2016-11-16 09:12:08 +0200

    Add index files to api for easier imports
    Refs #3.

---
+++ app/api/actions/index.js
@@ -0,0 +1,17 @@
+import {
+  favoriteResource,
+  fetchFavoritedResources,
+  fetchResource,
+  fetchResources,
+  unfavoriteResource,
+} from './resources';
+import { fetchUnits } from './units';
+
+export {
+  favoriteResource,
+  fetchFavoritedResources,
+  fetchResource,
+  fetchResources,
+  fetchUnits,
+  unfavoriteResource,
+};
