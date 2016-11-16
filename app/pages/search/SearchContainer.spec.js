94734cf474be5f3a2207ca54bd962ddd592055f0
Lauri Raivio <lauri.raivio@gmail.com>
2016-11-16 11:05:17 +0200

    Render resources on search page
    Renders resource name and a link to resource page.
    Refs #4.

--- app/pages/search/SearchContainer.spec.js
+++ app/pages/search/SearchContainer.spec.js
@@ -1,6 +1,8 @@
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import React from 'react';
+import Loader from 'react-loader';
+import { Link } from 'react-router';
 import simple from 'simple-mock';
 
 import { UnconnectedSearchContainer as SearchContainer } from './SearchContainer';
@@ -10,16 +12,54 @@ describe('pages/search/SearchContainer', () => {
     const defaults = {
       message: 'Some message',
       fetchResources: simple.mock(),
+      resources: [],
     };
     return shallow(<SearchContainer {...defaults} {...props} />);
   }
 
   describe('render', () => {
-    it('renders a header with correct text', () => {
+    describe('when resources are fetched', () => {
+      let wrapper;
       const message = 'Hello';
-      const header = getWrapper({ message }).find('h1');
-      expect(header.length).to.equal(1);
-      expect(header.text()).to.equal('Project Hello');
+      const resources = [
+        { id: 'r-1', name: { fi: 'Resurssi 1' } },
+        { id: 'r-2', name: { fi: 'Resurssi 2' } },
+      ];
+
+      before(() => {
+        wrapper = getWrapper({ message, resources });
+      });
+
+      it('renders a header with correct text', () => {
+        const header = wrapper.find('h1');
+        expect(header.length).to.equal(1);
+        expect(header.text()).to.equal('Project Hello');
+      });
+
+      it('renders a Link with correct props for each resource given in props', () => {
+        const links = wrapper.find(Link);
+
+        expect(links).to.have.length(resources.length);
+        links.forEach((link, index) => {
+          const resource = resources[index];
+          expect(link.prop('to')).to.equal(`/resources/${resource.id}`);
+          expect(link.prop('children')).to.equal(resource.name.fi);
+        });
+      });
+    });
+
+    describe('when resources are not fetched', () => {
+      let wrapper;
+      const resources = [];
+
+      before(() => {
+        wrapper = getWrapper({ resources });
+      });
+
+      it('renders a Loader', () => {
+        const loader = wrapper.find(Loader);
+        expect(loader).to.have.length(1);
+      });
     });
   });
 
--- app/pages/search/searchSelector.js
+++ app/pages/search/searchSelector.js
@@ -1,9 +1,15 @@
+import values from 'lodash/values';
 import { createStructuredSelector } from 'reselect';
 
 function messageSelector(state) {
   return state.search.message;
 }
 
+function resourcesSelector(state) {
+  return values(state.data.resources);
+}
+
 export default createStructuredSelector({
   message: messageSelector,
+  resources: resourcesSelector,
 });
