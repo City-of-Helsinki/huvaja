40fc994e7f275a658f9fa83b2ad2efd39e7cb90e
Lauri Raivio <lauri.raivio@gmail.com>
2016-11-16 11:05:17 +0200

    Render resources on search page
    Renders resource name and a link to resource page.
    Refs #4.

--- app/pages/search/SearchContainer.js
+++ app/pages/search/SearchContainer.js
@@ -1,9 +1,20 @@
 import React, { Component, PropTypes } from 'react';
+import Loader from 'react-loader';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 
 import { fetchResources } from 'api/actions';
 import selector from './searchSelector';
+
+function renderResource(resource) {
+  return (
+    <li key={resource.id}>
+      <Link to={`/resources/${resource.id}`}>
+        {resource.name.fi}
+      </Link>
+    </li>
+  );
+}
 
 export class UnconnectedSearchContainer extends Component {
   componentDidMount() {
@@ -11,18 +22,33 @@ export class UnconnectedSearchContainer extends Component {
   }
 
   render() {
+    const { message, resources } = this.props;
+    const isLoaded = Boolean(resources.length);
+
     return (
       <div className="search">
-        <h1>Project {this.props.message}</h1>
-        <Link to="/resources/999">Go to resource page</Link>
+        <Loader loaded={isLoaded}>
+          <h1>Project {message}</h1>
+          <ul>
+            {resources.map(renderResource)}
+          </ul>
+        </Loader>
       </div>
     );
   }
 }
 
+const resourceShape = PropTypes.shape({
+  id: PropTypes.string.isRequired,
+  name: PropTypes.shape({
+    fi: PropTypes.string.isRequired,
+  }).isRequired,
+});
+
 UnconnectedSearchContainer.propTypes = {
   fetchResources: PropTypes.func.isRequired,
   message: PropTypes.string.isRequired,
+  resources: PropTypes.arrayOf(resourceShape).isRequired,
 };
 
 const actions = {
