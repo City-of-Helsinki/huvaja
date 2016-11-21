44bfddfe3aaea62c7ef82a4c84f366ec19254750
Lauri Raivio <lauri.raivio@gmail.com>
2016-11-21 13:04:28 +0200

    Add createApiAction utility function
    Refs #10.

--- app/api/actions/units.js
+++ app/api/actions/units.js
@@ -1,33 +1,14 @@
-import { CALL_API } from 'redux-api-middleware';
-
-import types from '../actionTypes';
 import schemas from './schemas';
-import {
-  buildAPIUrl,
-  getErrorTypeDescriptor,
-  getHeadersCreator,
-  getRequestTypeDescriptor,
-  getSuccessTypeDescriptor,
-} from './utils';
+import { createApiAction } from './utils';
 
 function fetchUnits() {
-  const fetchParams = { pageSize: 100 };
-
-  return {
-    [CALL_API]: {
-      types: [
-        getRequestTypeDescriptor(types.UNITS_GET_REQUEST),
-        getSuccessTypeDescriptor(
-          types.UNITS_GET_SUCCESS,
-          { schema: schemas.paginatedUnitsSchema }
-        ),
-        getErrorTypeDescriptor(types.UNITS_GET_ERROR),
-      ],
-      endpoint: buildAPIUrl('unit', fetchParams),
-      method: 'GET',
-      headers: getHeadersCreator(),
-    },
-  };
+  return createApiAction({
+    endpoint: 'unit',
+    params: { pageSize: 100 },
+    method: 'GET',
+    type: 'UNITS',
+    options: { schema: schemas.paginatedUnitsSchema },
+  });
 }
 
 export {
