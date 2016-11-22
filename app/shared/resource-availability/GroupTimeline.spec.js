c400a9e6f828b49da03f0e84b9ddcd2cb3ee2885
Oskari Hiltunen <felix.loether@gmail.com>
2016-11-22 15:02:34 +0200

    Add GroupTimeline
    Refs #9.

---
+++ app/shared/resource-availability/GroupTimeline.spec.js
@@ -0,0 +1,71 @@
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import moment from 'moment';
+import React from 'react';
+
+import GroupTimeline from './GroupTimeline';
+import ResourceReservationsTimelineContainer from './ResourceReservationsTimelineContainer';
+
+function getWrapper(props) {
+  const defaults = {
+    date: moment('2016-01-01T00:00:00'),
+    resources: [],
+  };
+  return shallow(<GroupTimeline {...defaults} {...props} />);
+}
+
+describe('shared/resource-availability/GroupTimeline', () => {
+  it('renders a div.group-timeline', () => {
+    const wrapper = getWrapper();
+    expect(wrapper.is('div.group-timeline')).to.be.true;
+  });
+
+  it('renders hours', () => {
+    const hours = getWrapper().find('.hour');
+    expect(hours).to.have.length(24);
+    const texts = hours.map(hour => hour.text());
+    expect(texts).to.deep.equal([
+      '00:00',
+      '01:00',
+      '02:00',
+      '03:00',
+      '04:00',
+      '05:00',
+      '06:00',
+      '07:00',
+      '08:00',
+      '09:00',
+      '10:00',
+      '11:00',
+      '12:00',
+      '13:00',
+      '14:00',
+      '15:00',
+      '16:00',
+      '17:00',
+      '18:00',
+      '19:00',
+      '20:00',
+      '21:00',
+      '22:00',
+      '23:00',
+    ]);
+  });
+
+  it('renders no resources if none given', () => {
+    const wrapper = getWrapper({ resources: [] });
+    const resources = wrapper.find(ResourceReservationsTimelineContainer);
+    expect(resources).to.have.length(0);
+  });
+
+  it('renders resources', () => {
+    const resources = ['1234', '5678', '90ab', 'cdef'];
+    const wrapper = getWrapper({ resources });
+    const elements = wrapper.find(ResourceReservationsTimelineContainer);
+    expect(elements).to.have.length(4);
+    expect(elements.at(0).prop('id')).to.equal(resources[0]);
+    expect(elements.at(1).prop('id')).to.equal(resources[1]);
+    expect(elements.at(2).prop('id')).to.equal(resources[2]);
+    expect(elements.at(3).prop('id')).to.equal(resources[3]);
+  });
+});
