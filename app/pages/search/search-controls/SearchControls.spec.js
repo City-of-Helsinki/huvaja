f83d86fb6bdd20a94c181e9967c21414a79d064a
Lauri Raivio <lauri.raivio@gmail.com>
2016-11-23 09:11:25 +0200

    Change search page url when search is done
    Search page url is updated with the current search filters.
    Refs #16.

--- app/pages/search/search-controls/SearchControls.spec.js
+++ app/pages/search/search-controls/SearchControls.spec.js
@@ -1,9 +1,12 @@
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
+import queryString from 'query-string';
 import React from 'react';
 import Button from 'react-bootstrap/lib/Button';
 import ControlLabel from 'react-bootstrap/lib/ControlLabel';
 import FormControl from 'react-bootstrap/lib/FormControl';
+import { browserHistory } from 'react-router';
+import simple from 'simple-mock';
 
 import SearchControls from './SearchControls';
 
@@ -47,5 +50,33 @@ describe('pages/search/search-controls/SearchControls', () => {
     const button = getWrapper().find(Button);
     expect(button).to.have.length(1);
     expect(button.prop('type')).to.equal('submit');
+  });
+
+  describe('handleSearch', () => {
+    const searchFilters = {
+      query: 'some query',
+    };
+    const mockSubmitEvent = { preventDefault: () => {} };
+    let browserHistoryMock;
+    let instance;
+
+    before(() => {
+      browserHistoryMock = simple.mock(browserHistory, 'push');
+      instance = getWrapper().instance();
+      instance.state = searchFilters;
+      instance.handleSearch(mockSubmitEvent);
+    });
+
+    after(() => {
+      simple.restore();
+    });
+
+    it('changes the url with current search filters', () => {
+      const actualPath = browserHistoryMock.lastCall.args[0];
+      const expectedPath = `/?${queryString.stringify(searchFilters)}`;
+
+      expect(browserHistoryMock.callCount).to.equal(1);
+      expect(actualPath).to.equal(expectedPath);
+    });
   });
 });
