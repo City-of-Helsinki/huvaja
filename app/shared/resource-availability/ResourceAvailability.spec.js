dcd8cc890c48d2c2b1c2868ccc8ae933490794ae
Oskari Hiltunen <felix.loether@gmail.com>
2016-11-23 12:30:48 +0200

    Add ResourceAvailability
    Refs #9.

---
+++ app/shared/resource-availability/ResourceAvailability.spec.js
@@ -0,0 +1,38 @@
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import React from 'react';
+
+import GroupedTimeline from './GroupedTimeline';
+import ResourceAvailability from './ResourceAvailability';
+import Sidebar from './Sidebar';
+
+function getWrapper(props) {
+  const defaults = {
+    date: {},
+    groups: [],
+  };
+  return shallow(<ResourceAvailability {...defaults} {...props} />);
+}
+
+describe('shared/resource-availability/ResourceAvailability', () => {
+  it('renders a div.resource-availability', () => {
+    const wrapper = getWrapper();
+    expect(wrapper.is('div.resource-availability')).to.be.true;
+  });
+
+  it('renders Sidebar', () => {
+    const groups = [];
+    const element = getWrapper({ groups }).find(Sidebar);
+    expect(element).to.have.length(1);
+    expect(element.prop('groups')).to.equal(groups);
+  });
+
+  it('renders GroupedTimeline', () => {
+    const date = {};
+    const groups = [];
+    const element = getWrapper({ date, groups }).find(GroupedTimeline);
+    expect(element).to.have.length(1);
+    expect(element.prop('date')).to.equal(date);
+    expect(element.prop('groups')).to.equal(groups);
+  });
+});
