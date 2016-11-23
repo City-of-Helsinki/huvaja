74b96feb249dcbdef1fc3e7a56844ce64df18993
Oskari Hiltunen <felix.loether@gmail.com>
2016-11-23 12:30:48 +0200

    Add ResourceAvailability
    Refs #9.

--- app/shared/resource-availability/ResourceAvailability.js
+++ app/shared/resource-availability/ResourceAvailability.js
@@ -1,10 +1,24 @@
 import React, { PropTypes } from 'react';
 
-ResourceAvailability.propTypes = {
-  date: PropTypes.string.isRequired,
-};
+import GroupedTimeline from './GroupedTimeline';
+import Sidebar from './Sidebar';
 
+ResourceAvailability.propTypes = {
+  date: PropTypes.object.isRequired,
+  groups: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 export default function ResourceAvailability(props) {
-  return <div>{props.date}</div>;
+  return (
+    <div className="resource-availability">
+      <div className="left">
+        <div className="top-left" />
+        <Sidebar groups={props.groups} />
+      </div>
+      <div className="right">
+        <div className="date-selector">TODO</div>
+        <GroupedTimeline date={props.date} groups={props.groups} />
+      </div>
+    </div>
+  );
 }
 
