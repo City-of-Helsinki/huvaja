bb31449fc5043a28a8ec746b3cc3379b2c3cb2b9
Lauri Raivio <lauri.raivio@gmail.com>
2016-11-23 08:54:42 +0200

    Dispatch action whenever route changes
    Dispatches an action with type 'ENTER_OR_CHANGE_[component-name]_PAGE' and
    with the new location as the payload whenever a Route Component enters
    or changes.
    The action can be used in reducers to sync query parameters in url with the
    state.
    Refs #16.

--- app/routes.js
+++ app/routes.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
+import { createAction } from 'redux-actions';
 
 import AppContainer from 'pages/AppContainer';
 import SearchPage from 'pages/search';
 import ResourcePage from 'pages/resource';
+import store from 'state/store';
+
+function getDispatchers(componentName) {
+  const routeChangedAction = createAction(`ENTER_OR_CHANGE_${componentName.toUpperCase()}_PAGE`);
+
+  function onChange(prevState, nextState) {
+    store.dispatch(routeChangedAction(nextState.location));
+  }
+
+  function onEnter(nextState) {
+    store.dispatch(routeChangedAction(nextState.location));
+  }
+
+  return { onChange, onEnter };
+}
+
 
 export default (
   <Route>
     <Route component={AppContainer} path="/">
-      <IndexRoute component={SearchPage} />
-      <Route component={ResourcePage} path="/resources/:id" />
+      <IndexRoute component={SearchPage} {...getDispatchers('SEARCH')} />
+      <Route component={ResourcePage} path="/resources/:id" {...getDispatchers('RESOURCE')} />
     </Route>
   </Route>
 );
