12fb1d23a9edeaf655f0b67e50cbb06620a06b78
Raúl García <serulo@gmail.com>
2016-11-23 18:09:42 +0100

    Add more resourceInfo fields (Refs #18)

--- app/pages/resource/info/ResourceInfo.js
+++ app/pages/resource/info/ResourceInfo.js
@@ -1,13 +1,34 @@
 import React, { PropTypes } from 'react';
+import Col from 'react-bootstrap/lib/Col';
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
 
 function ResourceInfo({ resource, unit }) {
   return (
     <div className="resource-info">
-      <h1>
-        <span className="unit-name">{unit.name.fi}</span>
-        {' '}
-        <span className="resource-name">{resource.name.fi}</span>
-      </h1>
+      <header>
+        <h2 className="unit-name">{unit.name.fi}</h2>
+        <h1 className="resource-name">{resource.name.fi}</h1>
+        <h4 className="unit-address">{unit.streetAddress.fi}</h4>
+      </header>
+      <section className="resource-details">
+        <Grid>
+          <Row>
+            <Col xs={12} sm={6}>
+              <figure />
+            </Col>
+            <Col xs={12} sm={6}>
+              <aside>
+                <h3 className="resource-type">{resource.type.name.fi}</h3>
+                <div className="details-row resource-people-capacity">
+                  <span className="details-label">Henkilömäärä: </span>
+                  <span className="details-value">{resource.peopleCapacity}</span>
+                </div>
+              </aside>
+            </Col>
+          </Row>
+        </Grid>
+      </section>
     </div>
   );
 }
--- app/pages/resource/info/ResourceInfo.spec.js
+++ app/pages/resource/info/ResourceInfo.spec.js
@@ -7,9 +7,12 @@ import ResourceInfo from './ResourceInfo';
 describe('pages/resource/info/ResourceInfo', () => {
   const resource = {
     name: { fi: 'Huone' },
+    type: { name: { fi: 'Työpiste' } },
+    peopleCapacity: 3,
   };
   const unit = {
     name: { fi: 'Rakennus' },
+    streetAddress: { fi: 'Testinkatu' },
   };
 
   function getWrapper(props) {
@@ -20,13 +23,41 @@ describe('pages/resource/info/ResourceInfo', () => {
     return shallow(<ResourceInfo {...defaults} {...props} />);
   }
 
-  it('renders unit name', () => {
-    const unitName = getWrapper().find('.unit-name');
-    expect(unitName.text()).to.equal(unit.name.fi);
-  });
+  describe('header', () => {
+    let header;
+    before(() => {
+      header = getWrapper().find('header');
+    });
 
-  it('renders resource name', () => {
-    const resourceName = getWrapper().find('.resource-name');
-    expect(resourceName.text()).to.equal(resource.name.fi);
+    it('renders unit name', () => {
+      const unitName = header.find('.unit-name');
+      expect(unitName.text()).to.equal(unit.name.fi);
+    });
+
+    it('renders resource name', () => {
+      const resourceName = header.find('.resource-name');
+      expect(resourceName.text()).to.equal(resource.name.fi);
+    });
+
+    it('renders resource address', () => {
+      const unitAddress = header.find('.unit-address');
+      expect(unitAddress.text()).to.equal(unit.streetAddress.fi);
+    });
+  });
+  describe('resource-details section', () => {
+    let section;
+    before(() => {
+      section = getWrapper().find('section.resource-details');
+    });
+
+    it('renders resource type', () => {
+      const resourceType = section.find('.resource-type');
+      expect(resourceType.text()).to.equal(resource.type.name.fi);
+    });
+
+    it('renders resource people capacity', () => {
+      const resourceCapacity = section.find('.resource-people-capacity');
+      expect(resourceCapacity.text()).to.equal(`Henkilömäärä: ${resource.peopleCapacity}`);
+    });
   });
 });
