b13e1ea8fcb870bf9573f400686dccf9cd2d69e1
Raúl García <serulo@gmail.com>
2016-11-24 15:23:07 +0100

    Remove Grid around Row in ResourceInfo
    Refs #18.

--- app/pages/resource/info/ResourceInfo.js
+++ app/pages/resource/info/ResourceInfo.js
@@ -1,7 +1,6 @@
 import React, { PropTypes } from 'react';
 import Col from 'react-bootstrap/lib/Col';
 import Glyphicon from 'react-bootstrap/lib/Glyphicon';
-import Grid from 'react-bootstrap/lib/Grid';
 import Row from 'react-bootstrap/lib/Row';
 
 import ImageCarousel from 'shared/image-carousel';
@@ -18,25 +17,23 @@ function ResourceInfo({ resource, unit }) {
         </h4>
       </header>
       <section className="resource-details">
-        <Grid>
-          <Row>
-            <Col xs={12} sm={7}>
-              <ImageCarousel images={resource.images} />
-            </Col>
-            <Col xs={12} sm={5}>
-              <aside>
-                <h3 className="resource-type">{resource.type.name.fi}</h3>
-                <div className="details-row resource-people-capacity">
-                  <span className="details-label">Henkilömäärä: </span>
-                  <span className="details-value">{resource.peopleCapacity}</span>
-                </div>
-                <div className="resource-description">
-                  <WrappedText text={resource.description.fi} />
-                </div>
-              </aside>
-            </Col>
-          </Row>
-        </Grid>
+        <Row>
+          <Col xs={12} sm={7}>
+            <ImageCarousel images={resource.images} />
+          </Col>
+          <Col xs={12} sm={5}>
+            <aside>
+              <h3 className="resource-type">{resource.type.name.fi}</h3>
+              <div className="details-row resource-people-capacity">
+                <span className="details-label">Henkilömäärä: </span>
+                <span className="details-value">{resource.peopleCapacity}</span>
+              </div>
+              <div className="resource-description">
+                <WrappedText text={resource.description.fi} />
+              </div>
+            </aside>
+          </Col>
+        </Row>
       </section>
     </div>
   );
