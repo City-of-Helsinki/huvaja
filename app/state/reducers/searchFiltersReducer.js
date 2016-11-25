c8deeb5bad8199f4e95e0887f56e608fab0290c5
Lauri Raivio <lauri.raivio@gmail.com>
2016-11-25 09:19:05 +0200

    Add date to search filters
    Refs #20.

--- app/state/reducers/searchFiltersReducer.js
+++ app/state/reducers/searchFiltersReducer.js
@@ -1,12 +1,16 @@
+import moment from 'moment';
 import { handleActions } from 'redux-actions';
 
-const initialState = {
-  search: '',
-};
+function getInitialState() {
+  return {
+    date: moment().format('YYYY-MM-DD'),
+    search: '',
+  };
+}
 
 export default handleActions({
   ENTER_OR_CHANGE_SEARCH_PAGE: (state, action) => {
     const filters = action.payload.query;
-    return { ...initialState, ...filters };
+    return { ...getInitialState(), ...filters };
   },
-}, initialState);
+}, getInitialState());
