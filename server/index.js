3ede5bfe0bc6cc66332ac3d1645b0e3930c4658b
Lauri Raivio <lauri.raivio@gmail.com>
2016-11-29 17:22:15 +0200

    Add dotenv to development
    Refs #34.

--- server/index.js
+++ server/index.js
@@ -5,5 +5,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 if (isProduction) {
   throw new Error('Production should only use Nginx, not Express.');
 } else {
+  const path = require('path');
+  require('dotenv').load({ path: path.resolve(__dirname, '../.env') });
   require('./development-server');
 }
