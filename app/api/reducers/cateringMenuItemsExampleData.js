cabe71164de60e9f7b947df7b548df56484c7472
Lauri Raivio <lauri.raivio@gmail.com>
2016-12-06 12:45:21 +0200

    Add cateringMenuItems example data
    Refs #42.

---
+++ app/api/reducers/cateringMenuItemsExampleData.js
@@ -0,0 +1,27 @@
+export default {
+  'cmi-1': {
+    id: 'cmi-1',
+    name: 'Kahvi ja tee',
+    price: 1.0,
+  },
+  'cmi-2': {
+    id: 'cmi-2',
+    name: 'Päivän pulla',
+    price: 2.0,
+  },
+  'cmi-3': {
+    id: 'cmi-3',
+    name: 'Hedelmätarjoilu',
+    price: 2.5,
+  },
+  'cmi-4': {
+    id: 'cmi-4',
+    name: 'Voileipä',
+    price: 3.0,
+  },
+  'cmi-5': {
+    id: 'cmi-5',
+    name: 'Virvoitusjuomavalikoima',
+    price: 3.8,
+  },
+};
