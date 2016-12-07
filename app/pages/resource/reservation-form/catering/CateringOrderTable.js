2a3827e930aa24a753a25ac247e2342d1f5c0b07
Lauri Raivio <lauri.raivio@gmail.com>
2016-12-07 12:44:24 +0200

    Add CateringOrderTable to catering
    Displays selected catering menu items, quantities and prices.
    Refs #42.

---
+++ app/pages/resource/reservation-form/catering/CateringOrderTable.js
@@ -0,0 +1,45 @@
+import React, { PropTypes } from 'react';
+import Table from 'react-bootstrap/lib/Table';
+
+CateringOrderTable.propTypes = {
+  items: PropTypes.array.isRequired,
+};
+
+function CateringOrderTable(props) {
+  const totalPrice = props.items.reduce((sum, item) => sum + (item.quantity * item.price), 0);
+  return (
+    <Table className="catering-order-table" striped>
+      <thead>
+        <tr>
+          <th className="product">Tuote</th>
+          <th className="number">Hinta</th>
+          <th className="number">Määrä</th>
+          <th className="number">Yhteensä</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.items.map(item =>
+          <tr key={item.id}>
+            <td className="product">{item.name}</td>
+            <td className="number">{item.price.toFixed(2)} €</td>
+            <td className="number">{item.quantity}</td>
+            <td className="number">{(item.quantity * item.price).toFixed(2)} €</td>
+          </tr>
+        )}
+      </tbody>
+      {Boolean(totalPrice) &&
+        <tfoot>
+          <tr>
+            <th className="product">Yhteensä</th>
+            <th />
+            <th />
+            <th className="number">{totalPrice.toFixed(2)} €</th>
+          </tr>
+        </tfoot>
+      }
+    </Table>
+  );
+}
+
+
+export default CateringOrderTable;
