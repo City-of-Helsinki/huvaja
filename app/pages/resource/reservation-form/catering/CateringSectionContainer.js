ba20a9ff1bc7c24d48859a5ec0c6724fda5e949d
Lauri Raivio <lauri.raivio@gmail.com>
2016-12-07 15:20:15 +0200

    Show catering order in the reservation form
    Refs #42.

--- app/pages/resource/reservation-form/catering/CateringSectionContainer.js
+++ app/pages/resource/reservation-form/catering/CateringSectionContainer.js
@@ -4,15 +4,19 @@ import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
 
 import CateringModal from './CateringModal';
+import CateringOrderTable from './CateringOrderTable';
+import cateringUtils from './utils';
 
 export const selector = createSelector(
-  state => state.order || [],
-  order => ({ order })
+  state => state.catering,
+  state => state.data.cateringMenuItems,
+  (cateringData, cateringMenuItems) => ({ cateringData, cateringMenuItems })
 );
 
 class CateringSectionContainer extends Component {
   static propTypes = {
-    order: PropTypes.array.isRequired,
+    cateringData: PropTypes.object.isRequired,
+    cateringMenuItems: PropTypes.object.isRequired,
   }
 
   constructor(props) {
@@ -33,12 +37,21 @@ class CateringSectionContainer extends Component {
   }
 
   render() {
-    const { order } = this.props;
+    const { cateringData, cateringMenuItems } = this.props;
+    const orderItems = cateringUtils.getOrderItems(cateringMenuItems, cateringData.order);
+    const orderMade = Boolean(orderItems.length);
+
     return (
       <div className="catering-section">
         <h3>Tarjoilut</h3>
+        {!orderMade &&
+          <p>Ei tarjoilua</p>
+        }
+        {orderMade &&
+          <CateringOrderTable items={orderItems} />
+        }
         <Button onClick={this.openCateringModal}>
-          {order.length ? 'Muokkaa tarjoiluja' : 'Tilaa tarjoilut'}
+          {orderMade ? 'Muokkaa tarjoiluja' : 'Tilaa tarjoilut'}
         </Button>
         <CateringModal onClose={this.closeCateringModal} show={this.state.showCateringModal} />
       </div>
