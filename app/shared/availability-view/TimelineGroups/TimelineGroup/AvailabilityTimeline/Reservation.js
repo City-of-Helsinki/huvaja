30ab12a494ca9e911bc0e241cdbdc459f77aff5a
Lauri Raivio <lauri.raivio@gmail.com>
2016-12-08 21:46:46 +0200

    Render reservation data in popover
    Renders most important reservation data in popover.
    Refs #41.

--- app/shared/availability-view/TimelineGroups/TimelineGroup/AvailabilityTimeline/Reservation.js
+++ app/shared/availability-view/TimelineGroups/TimelineGroup/AvailabilityTimeline/Reservation.js
@@ -1,33 +1,49 @@
 import moment from 'moment';
 import React, { PropTypes } from 'react';
+import Glyphicon from 'react-bootstrap/lib/Glyphicon';
 import OverlayTrigger from 'react-bootstrap/lib/OverlayTrigger';
-import Tooltip from 'react-bootstrap/lib/Tooltip';
+import Popover from 'react-bootstrap/lib/Popover';
 
 import utils from '../utils';
 
 Reservation.propTypes = {
-  id: PropTypes.number.isRequired,
-  end: PropTypes.string.isRequired,
-  name: PropTypes.string,
   begin: PropTypes.string.isRequired,
+  end: PropTypes.string.isRequired,
+  eventSubject: PropTypes.string,
+  id: PropTypes.number.isRequired,
+  numberOfParticipants: PropTypes.number,
+  reserverName: PropTypes.string,
 };
+
 export default function Reservation(props) {
   const startTime = moment(props.begin);
   const endTime = moment(props.end);
   const width = utils.getTimeSlotWidth({ startTime, endTime });
-  const tooltip = (
-    <Tooltip id={`tooltip-${props.id}`}>
-      {startTime.format('HH:mm')} - {endTime.format('HH:mm')} {props.name}
-    </Tooltip>
+  const popover = (
+    <Popover id={`popover-${props.id}`} title={props.eventSubject}>
+      <div>
+        <Glyphicon glyph="time" />{' '}
+        {startTime.format('HH:mm')} - {endTime.format('HH:mm')}
+      </div>
+      <div>{props.reserverName}</div>
+      <div><Glyphicon glyph="user" /> {props.numberOfParticipants}</div>
+    </Popover>
   );
   return (
-    <OverlayTrigger overlay={tooltip}>
+    <OverlayTrigger
+      overlay={popover}
+      placement="top"
+      trigger={['hover', 'focus']}
+    >
       <div className="reservation" style={{ width }}>
         <div className="times">
           <div className="start-time">{startTime.format('HH:mm')}</div>
           <div className="end-time">{endTime.format('HH:mm')}</div>
         </div>
-        <div className="name">{props.name}</div>
+        <div className="names">
+          <div className="event-subject">{props.eventSubject}</div>
+          <div className="reserver-name">{props.reserverName}</div>
+        </div>
       </div>
     </OverlayTrigger>
   );
--- app/shared/availability-view/TimelineGroups/TimelineGroup/AvailabilityTimeline/Reservation.spec.js
+++ app/shared/availability-view/TimelineGroups/TimelineGroup/AvailabilityTimeline/Reservation.spec.js
@@ -3,17 +3,17 @@ import { shallow } from 'enzyme';
 import moment from 'moment';
 import React from 'react';
 import OverlayTrigger from 'react-bootstrap/lib/OverlayTrigger';
-import Tooltip from 'react-bootstrap/lib/Tooltip';
+import Popover from 'react-bootstrap/lib/Popover';
 
 import Reservation from './Reservation';
 import utils from '../utils';
 
 function getOverlayTrigger(props) {
   const defaults = {
-    id: 12345,
-    end: '2016-01-01T12:00:00Z',
-    name: 'Meeting',
     begin: '2016-01-01T10:00:00Z',
+    end: '2016-01-01T12:00:00Z',
+    eventSubject: 'Meeting',
+    id: 12345,
   };
   return shallow(<Reservation {...defaults} {...props} />);
 }
@@ -33,10 +33,10 @@ describe('shared/availability-view/Reservation', () => {
     expect(wrapper.is(OverlayTrigger)).to.be.true;
   });
 
-  it('renders a tooltip', () => {
+  it('renders a popover', () => {
     const overlayTrigger = getOverlayTrigger();
     const overlay = overlayTrigger.prop('overlay');
-    expect(overlay.type).to.equal(Tooltip);
+    expect(overlay.type).to.equal(Popover);
   });
 
   it('has correct width', () => {
@@ -66,10 +66,17 @@ describe('shared/availability-view/Reservation', () => {
     expect(element.text()).to.equal(moment(end).format('HH:mm'));
   });
 
-  it('renders name', () => {
-    const name = 'Meeting GUQ';
-    const element = getWrapper({ name }).find('.name');
+  it('renders eventSubject', () => {
+    const eventSubject = 'Meeting GUQ';
+    const element = getWrapper({ eventSubject }).find('.event-subject');
     expect(element).to.have.length(1);
-    expect(element.text()).to.equal(name);
+    expect(element.text()).to.equal(eventSubject);
+  });
+
+  it('renders reserverName', () => {
+    const reserverName = 'Luke Skywalker';
+    const element = getWrapper({ reserverName }).find('.reserver-name');
+    expect(element).to.have.length(1);
+    expect(element.text()).to.equal(reserverName);
   });
 });
