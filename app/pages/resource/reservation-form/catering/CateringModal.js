82b47f97bc3d469d45e8714013aced2c71e29238
Lauri Raivio <lauri.raivio@gmail.com>
2016-12-08 13:54:06 +0200

    Improve and add tests for CateringModal
    Refs #42.

--- app/pages/resource/reservation-form/catering/CateringModal.js
+++ app/pages/resource/reservation-form/catering/CateringModal.js
@@ -3,23 +3,22 @@ import Modal from 'react-bootstrap/lib/Modal';
 
 import CateringForm from './catering-form';
 
+CateringModal.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  show: PropTypes.bool.isRequired,
+};
+
 function CateringModal(props) {
   return (
     <Modal className="catering-modal" onHide={props.onClose} show={props.show}>
       <Modal.Header closeButton>
         <Modal.Title>Lisää tarjoilutilaus</Modal.Title>
       </Modal.Header>
-
       <Modal.Body>
         <CateringForm onCancelCallback={props.onClose} onSubmitCallback={props.onClose} />
       </Modal.Body>
     </Modal>
   );
 }
-
-CateringModal.propTypes = {
-  onClose: PropTypes.func.isRequired,
-  show: PropTypes.bool.isRequired,
-};
 
 export default CateringModal;
