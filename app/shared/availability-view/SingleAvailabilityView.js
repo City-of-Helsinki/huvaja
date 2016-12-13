86fccf1c09d7e0a5d8ded9931e75a55f253ca9a8
Oskari Hiltunen <felix.loether@gmail.com>
2016-12-13 13:23:21 +0200

    Add dragscroll to SingleAvailabilityView
    Refs #83.

--- app/shared/availability-view/SingleAvailabilityView.js
+++ app/shared/availability-view/SingleAvailabilityView.js
@@ -1,28 +1,37 @@
+import dragscroll from 'dragscroll';
 import React, { PropTypes } from 'react';
 
 import DateSelector from './DateSelector';
 import TimelineGroup from './TimelineGroups/TimelineGroup';
 
-SingleAvailabilityView.propTypes = {
-  date: PropTypes.string.isRequired,
-  onDateChange: PropTypes.func.isRequired,
-  onReservationSlotClick: PropTypes.func,
-  resource: PropTypes.string.isRequired,
-  selection: PropTypes.shape({
-    begin: PropTypes.string.isRequired,
-    end: PropTypes.string.isRequired,
-  }),
-};
-export default function SingleAvailabilityView(props) {
-  return (
-    <div className="availability-view availability-view-single">
-      <DateSelector value={props.date} onChange={props.onDateChange} />
-      <TimelineGroup
-        date={props.date}
-        onReservationSlotClick={props.onReservationSlotClick}
-        resources={[props.resource]}
-        selection={props.selection}
-      />
-    </div>
-  );
+export default class SingleAvailabilityView extends React.Component {
+  static propTypes = {
+    date: PropTypes.string.isRequired,
+    onDateChange: PropTypes.func.isRequired,
+    onReservationSlotClick: PropTypes.func,
+    resource: PropTypes.string.isRequired,
+    selection: PropTypes.shape({
+      begin: PropTypes.string.isRequired,
+      end: PropTypes.string.isRequired,
+    }),
+  };
+
+  componentDidMount() {
+    dragscroll.reset();
+  }
+
+  render() {
+    return (
+      <div className="availability-view availability-view-single">
+        <DateSelector value={this.props.date} onChange={this.props.onDateChange} />
+        <TimelineGroup
+          className="dragscroll"
+          date={this.props.date}
+          onReservationSlotClick={this.props.onReservationSlotClick}
+          resources={[this.props.resource]}
+          selection={this.props.selection}
+        />
+      </div>
+    );
+  }
 }
