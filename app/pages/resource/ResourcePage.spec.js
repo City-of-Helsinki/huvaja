72dd5cd2da63ca8a1a38a2f5ecb7b6845c45d23f
Eero Vehmanen <eero.vehmanen@gmail.com>
2017-04-04 12:48:56 +0300

    Pass correct props to ResourceInfo (refs #95)
    - Current user is not needed anymore as it was only used to check if user was logged in or not. Currently only users who are logged in are able to see the page.
    - Modal actions are now needed

--- app/pages/resource/ResourcePage.spec.js
+++ app/pages/resource/ResourcePage.spec.js
@@ -7,24 +7,27 @@ import ResourceInfo from './info';
 import ResourcePage from './ResourcePage';
 import ReservationForm from './reservation-form';
 
-function getWrapper(props) {
+describe('pages/resource/ResourcePage', () => {
   const defaults = {
     date: '2016-01-01',
+    hideResourceImages: () => null,
     onDateChange: () => null,
     resource: { id: 'ham' },
+    showResourceImages: () => null,
     unit: { name: { fi: 'Foobar' } },
   };
-  return shallow(<ResourcePage {...defaults} {...props} />);
-}
 
-describe('pages/resource/ResourcePage', () => {
+  function getWrapper(props) {
+    return shallow(<ResourcePage {...defaults} {...props} />);
+  }
+
   it('renders ResourceInfo', () => {
-    const resource = { id: 'id' };
-    const unit = { name: { fi: 'unit' } };
-    const info = getWrapper({ resource, unit }).find(ResourceInfo);
+    const info = getWrapper().find(ResourceInfo);
     expect(info).to.have.length(1);
-    expect(info.prop('resource')).to.equal(resource);
-    expect(info.prop('unit')).to.equal(unit);
+    expect(info.prop('resource')).to.equal(defaults.resource);
+    expect(info.prop('unit')).to.equal(defaults.unit);
+    expect(info.prop('hideResourceImages')).to.equal(defaults.hideResourceImages);
+    expect(info.prop('showResourceImages')).to.equal(defaults.showResourceImages);
   });
 
   it('renders ReservationForm', () => {
