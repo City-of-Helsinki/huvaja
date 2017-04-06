c60bad28d0f515b12bfe8ddd0d31e235f1576fba
Eero Vehmanen <eero.vehmanen@gmail.com>
2017-04-06 12:40:44 +0300

    Use Helsinki timezone (refs #108)
    Previously user located for example in Sweden would see reservations in Sweden timezone which didn't make sense because the resources are located in Helsinki.

--- app/index.js
+++ app/index.js
@@ -1,4 +1,6 @@
 import 'moment/locale/fi';
+import 'moment-timezone';
+import moment from 'moment';
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
@@ -7,6 +9,8 @@ import { browserHistory, Router } from 'react-router';
 import 'assets/styles/app.less';
 import store from 'state/store';
 import routes from './routes';
+
+moment.tz.setDefault('Europe/Helsinki');
 
 render(
   <Provider store={store}>
