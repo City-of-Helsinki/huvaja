6f09c0e329ac826d707f67e70f6eb5495ace860a
Eero Vehmanen <eero.vehmanen@gmail.com>
2017-04-07 10:10:40 +0300

    Test searchPageSelector (refs #108)

--- app/pages/search/searchPageSelector.spec.js
+++ app/pages/search/searchPageSelector.spec.js
@@ -14,12 +14,30 @@ describe('pages/search/searchPageSelector', () => {
     'u-1': { id: 'u-1', name: { fi: 'Unit 1' } },
     'u-2': { id: 'u-2', name: { fi: 'Unit 2' } },
   };
-  const searchFilters = { date: '2016-12-12', search: 'search text', isFavorite: '' };
+  const equipment = {
+    'e-1': { id: 'e-1', name: { fi: 'Equipment 1' } },
+    'e-2': { id: 'e-2', name: { fi: 'Equipment 2' } },
+  };
+  const types = {
+    't-1': { id: 't-1', name: { fi: 'Type 1' } },
+    't-2': { id: 't-2', name: { fi: 'Type 2' } },
+  };
+  const searchFilters = {
+    date: '2016-12-12',
+    equipment: '',
+    isFavorite: '',
+    people: '',
+    search: 'search text',
+    type: '',
+    unit: '',
+  };
   const searchResults = ['r-1', 'r-2', 'r-3'];
 
   function getSelected() {
     const state = getState({
+      'data.equipment': equipment,
       'data.resources': resources,
+      'data.types': types,
       'data.units': units,
       'searchPage.searchFilters': searchFilters,
       'searchPage.searchResults': searchResults,
@@ -39,11 +57,23 @@ describe('pages/search/searchPageSelector', () => {
     expect(getSelected().availabilityGroups).to.deep.equal(expected);
   });
 
-  it('returns searchFilters form the state', () => {
+  it('returns searchFilters from the state', () => {
     expect(getSelected().searchFilters).to.deep.equal(searchFilters);
   });
 
-  it('returns resultsCount form the state', () => {
+  it('returns resultsCount from the state', () => {
     expect(getSelected().resultsCount).to.deep.equal(searchResults.length);
+  });
+
+  it('returns equipment from the state', () => {
+    expect(getSelected().equipment).to.deep.equal(equipment);
+  });
+
+  it('returns types from the state', () => {
+    expect(getSelected().types).to.deep.equal(types);
+  });
+
+  it('returns units from the state', () => {
+    expect(getSelected().units).to.deep.equal(units);
   });
 });
