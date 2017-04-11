6e72ab83567d7e69dbf03a3cc0ef199b956cdcd2
Eero Vehmanen <eero.vehmanen@gmail.com>
2017-04-11 14:58:48 +0300

    Modify search filters reducer (refs #117)
    Changing filters is now done with specific UI action, except that removing url params resets state.

--- app/state/reducers/searchFiltersReducer.js
+++ app/state/reducers/searchFiltersReducer.js
@@ -1,8 +1,8 @@
-import { camelizeKeys } from 'humps';
+import isEmpty from 'lodash/isEmpty';
 import moment from 'moment';
 import { handleActions } from 'redux-actions';
 
-function getInitialState() {
+export function getInitialState() {
   return {
     date: moment().format('YYYY-MM-DD'),
     equipment: '',
@@ -15,8 +15,12 @@ function getInitialState() {
 }
 
 export default handleActions({
+  CHANGE_RESOURCE_SEARCH_FILTERS: (state, action) => ({
+    ...state,
+    ...action.payload,
+  }),
   ENTER_OR_CHANGE_SEARCH_PAGE: (state, action) => {
-    const filters = action.payload.query;
-    return camelizeKeys({ ...getInitialState(), ...filters });
+    if (isEmpty(action.payload.query)) return getInitialState();
+    return state;
   },
 }, getInitialState());
--- app/state/reducers/searchFiltersReducer.spec.js
+++ app/state/reducers/searchFiltersReducer.spec.js
@@ -2,50 +2,100 @@ import { expect } from 'chai';
 import moment from 'moment';
 import { createAction } from 'redux-actions';
 
-import searchFiltersReducer from './searchFiltersReducer';
+import searchFiltersReducer, { getInitialState } from './searchFiltersReducer';
 
 describe('state/reducers/searchFiltersReducer', () => {
   describe('initial state', () => {
-    function getInitialState() {
+    function getInitial() {
       return searchFiltersReducer(undefined, { type: 'NOOP' });
     }
 
     it('date is the current date', () => {
       const expected = moment().format('YYYY-MM-DD');
-      expect(getInitialState().date).to.equal(expected);
+      expect(getInitial().date).to.equal(expected);
     });
 
     it('search is an empty string', () => {
-      expect(getInitialState().search).to.equal('');
+      expect(getInitial().search).to.equal('');
     });
 
     it('isFavorite is empty string', () => {
-      expect(getInitialState().isFavorite).to.equal('');
+      expect(getInitial().isFavorite).to.equal('');
     });
 
     it('equipment is empty string', () => {
-      expect(getInitialState().equipment).to.equal('');
+      expect(getInitial().equipment).to.equal('');
     });
 
     it('type is empty string', () => {
-      expect(getInitialState().type).to.equal('');
+      expect(getInitial().type).to.equal('');
     });
 
     it('unit is empty string', () => {
-      expect(getInitialState().unit).to.equal('');
+      expect(getInitial().unit).to.equal('');
     });
 
     it('people is empty string', () => {
-      expect(getInitialState().people).to.equal('');
+      expect(getInitial().people).to.equal('');
     });
   });
 
   describe('handling actions', () => {
+    describe('CHANGE_RESOURCE_SEARCH_FILTERS', () => {
+      const routeChangedAction = createAction('CHANGE_RESOURCE_SEARCH_FILTERS');
+
+      it('sets parameters in payload to filters', () => {
+        const currentState = {};
+        const payload = {
+          date: '2016-12-12',
+          equipment: '',
+          is_favorite: '',
+          people: '',
+          search: 'search text',
+          type: '',
+          unit: '',
+        };
+        const action = routeChangedAction(payload);
+        const nextState = searchFiltersReducer(currentState, action);
+        expect(nextState).to.deep.equal(payload);
+      });
+
+      it('overrides previous values of same filters', () => {
+        const payload = {
+          date: '2016-12-12',
+          isFavorite: '',
+          search: 'search text',
+        };
+        const action = routeChangedAction(payload);
+        const currentState = {
+          date: '2016-12-12',
+          equipment: '',
+          isFavorite: '',
+          people: '',
+          search: 'old-value',
+          type: '',
+          unit: '',
+        };
+        const nextState = searchFiltersReducer(currentState, action);
+        expect(nextState).to.deep.equal({
+          date: '2016-12-12',
+          equipment: '',
+          isFavorite: '',
+          people: '',
+          search: 'search text',
+          type: '',
+          unit: '',
+        });
+      });
+    });
+
     describe('ENTER_OR_CHANGE_SEARCH_PAGE', () => {
       const routeChangedAction = createAction('ENTER_OR_CHANGE_SEARCH_PAGE');
 
-      it('sets query parameters in payload to filters', () => {
-        const currentState = {};
+      it('returns current state when payload has query parameters', () => {
+        const currentState = {
+          isFavorite: 'true',
+        };
         const payload = {
           query: {
             date: '2016-12-12',
@@ -59,54 +109,19 @@ describe('state/reducers/searchFiltersReducer', () => {
         };
         const action = routeChangedAction(payload);
         const nextState = searchFiltersReducer(currentState, action);
-        expect(nextState).to.deep.equal({
-          date: '2016-12-12',
-          equipment: '',
-          isFavorite: '',
-          people: '',
-          search: 'search text',
-          type: '',
-          unit: '',
-        });
+        expect(nextState).to.deep.equal(currentState);
       });
 
-      it('overrides previous values of same filters', () => {
+      it('returns initial state when payload has no query parameters', () => {
+        const currentState = {
+          isFavorite: 'true',
+        };
         const payload = {
-          query: {
-            date: '2016-12-12',
-            is_favorite: '',
-            search: 'search text',
-          },
+          query: {},
         };
         const action = routeChangedAction(payload);
-        const currentState = { search: 'old-value' };
         const nextState = searchFiltersReducer(currentState, action);
-        expect(nextState).to.deep.equal({
-          date: '2016-12-12',
-          equipment: '',
-          isFavorite: '',
-          people: '',
-          search: 'search text',
-          type: '',
-          unit: '',
-        });
-      });
-
-      it('use initial reducer state for filters that are not in the payload', () => {
-        const payload = { query: { date: '2016-12-12' } };
-        const action = routeChangedAction(payload);
-        const currentState = { date: '2016-11-11' };
-        const nextState = searchFiltersReducer(currentState, action);
-        const expected = {
-          date: '2016-12-12',
-          equipment: '',
-          isFavorite: '',
-          people: '',
-          search: '',
-          type: '',
-          unit: '',
-        };
-        expect(nextState).to.deep.equal(expected);
+        expect(nextState).to.deep.equal(getInitialState());
       });
     });
   });
