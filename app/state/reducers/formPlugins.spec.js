67ba172cc2ae8774ce5ac7b085b70c1e2c2b7521
Oskari Hiltunen <felix.loether@gmail.com>
2017-04-19 13:56:17 +0300

    Fix saving reservation
    Refs #114.

--- app/state/reducers/formPlugins.spec.js
+++ app/state/reducers/formPlugins.spec.js
@@ -8,15 +8,19 @@ describe('state/reducers/formPlugins', () => {
     describe('SET_SUBMIT_SUCCEEDED', () => {
       const plugin = plugins.resourceReservation;
       const action = { type: formActions.SET_SUBMIT_SUCCEEDED };
+      const time = {
+        begin: { date: '2017-01-01', time: null },
+        end: { date: '2017-01-01', time: null },
+      };
 
       it('sets anyTouched to undefined', () => {
-        const initial = { anyTouched: true, values: {} };
+        const initial = { anyTouched: true, values: { time } };
         const actual = plugin(initial, action);
         expect(actual.anyTouched).to.be.undefined;
       });
 
       it('sets fields to undefined', () => {
-        const initial = { fields: { some: 'field' }, values: {} };
+        const initial = { fields: { some: 'field' }, values: { time } };
         const actual = plugin(initial, action);
         expect(actual.fields).to.be.undefined;
       });
@@ -27,6 +31,7 @@ describe('state/reducers/formPlugins', () => {
             hostName: 'Luke Skywalker',
             reserverName: 'Luke Skywalker',
             resource: '13',
+            time,
             some: 'value',
           },
         };
@@ -34,9 +39,27 @@ describe('state/reducers/formPlugins', () => {
           hostName: 'Luke Skywalker',
           reserverName: 'Luke Skywalker',
           resource: '13',
+          time,
         };
         const actual = plugin(initial, action);
         expect(actual.values).to.deep.equal(expectedValues);
+      });
+
+      it('resets time but not date', () => {
+        const date = '2017-01-01';
+        const initial = {
+          values: {
+            time: {
+              begin: { date, time: '10:00' },
+              end: { date, time: '12:00' },
+            },
+          },
+        };
+        const actual = plugin(initial, action);
+        expect(actual.values.time).to.deep.equal({
+          begin: { date, time: null },
+          end: { date, time: null },
+        });
       });
     });
   });
