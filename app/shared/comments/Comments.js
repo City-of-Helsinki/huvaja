35c367ec8b0746382355a2d66c6beda310ad44fb
Oskari Hiltunen <felix.loether@gmail.com>
2017-04-25 13:34:44 +0300

    Add Comments component
    Refs #141.

---
+++ app/shared/comments/Comments.js
@@ -0,0 +1,32 @@
+import moment from 'moment';
+import React, { PropTypes } from 'react';
+
+function formatCreatedAt(datetime) {
+  return moment(datetime).format('D.M.YYYY HH.mm');
+}
+
+Comments.propTypes = {
+  comments: PropTypes.arrayOf(PropTypes.shape({
+    content: PropTypes.string.isRequired,
+    createdAt: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    user: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+    }).isRequired,
+  })).isRequired,
+};
+export default function Comments(props) {
+  return (
+    <div className="comments">
+      {props.comments.map(comment => (
+        <div className="comments-comment" key={comment.id}>
+          <div className="comments-comment-header">
+            <div className="comments-comment-date">{formatCreatedAt(comment.createdAt)}</div>
+            <div className="comments-comment-user">{comment.user.name}</div>
+          </div>
+          <div className="comments-comment-content">{comment.content}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
---
+++ app/shared/comments/Comments.spec.js
@@ -0,0 +1,59 @@
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import React from 'react';
+
+import Comments from './Comments';
+
+function getWrapper(props) {
+  const defaults = {
+    comments: [],
+  };
+  return shallow(<Comments {...defaults} {...props} />);
+}
+
+describe('shared/comments/Comments', () => {
+  it('renders a div.comments', () => {
+    const wrapper = getWrapper();
+    expect(wrapper.is('div.comments')).to.be.true;
+  });
+
+  it('renders no comments if no comments', () => {
+    const comments = getWrapper({ comments: [] }).find('.comments-comment');
+    expect(comments).to.have.length(0);
+  });
+
+  describe('comment', () => {
+    const comment = {
+      id: 1,
+      content: 'This comment is in the bag',
+      createdAt: '2017-02-03T12:34:56.789+02:00',
+      user: { name: 'Kunkka Shakearther' },
+    };
+
+    function getComment() {
+      return getWrapper({ comments: [comment] }).find('.comments-comment');
+    }
+
+    it('is rendered', () => {
+      expect(getComment()).to.have.length(1);
+    });
+
+    it('renders date', () => {
+      const date = getComment().find('.comments-comment-date');
+      expect(date).to.have.length(1);
+      expect(date.text()).to.equal('3.2.2017 12.34');
+    });
+
+    it('renders user name', () => {
+      const name = getComment().find('.comments-comment-user');
+      expect(name).to.have.length(1);
+      expect(name.text()).to.equal(comment.user.name);
+    });
+
+    it('renders comment content', () => {
+      const content = getComment().find('.comments-comment-content');
+      expect(content).to.have.length(1);
+      expect(content.text()).to.equal(comment.content);
+    });
+  });
+});
