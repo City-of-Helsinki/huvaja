55c4e8cdb18560f09f7a397397735ddd8ea67171
Oskari Hiltunen <felix.loether@gmail.com>
2017-04-25 20:35:23 +0300

    Add toggling comments
    Refs #141.

--- app/shared/comments/CommentsContainer.js
+++ app/shared/comments/CommentsContainer.js
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux';
 import React, { PropTypes } from 'react';
+import FontAwesome from 'react-fontawesome';
 import { createStructuredSelector } from 'reselect';
 
 import { fetchComments } from 'api/actions';
@@ -38,17 +39,41 @@ export const actions = {
 
 export class CommentsContainer extends React.Component {
   static propTypes = {
-    comments: PropTypes.array.isRequired,
+    comments: PropTypes.array,
     fetchComments: PropTypes.func.isRequired,
+    name: PropTypes.string.isRequired,
     reservationId: PropTypes.number.isRequired,
   }
+  state = { isOpen: false }
 
   componentWillMount() {
     this.props.fetchComments({ reservationId: this.props.reservationId });
   }
 
+  getCommentCount() {
+    if (!this.props.comments) return '-';
+    return this.props.comments.length;
+  }
+
+  toggleComments = (event) => {
+    event.preventDefault();
+    const canToggle = this.state.isOpen || (this.props.comments && this.props.comments.length);
+    if (canToggle) {
+      this.setState({ isOpen: !this.state.isOpen });
+    }
+  }
+
   render() {
-    return <Comments comments={this.props.comments} />;
+    return (
+      <div className="comments-container">
+        <a onClick={this.toggleComments} tabIndex="0">
+          {this.props.name} ({this.getCommentCount()})
+          {' '}
+          <FontAwesome name={this.state.isOpen ? 'caret-down' : 'caret-right'} />
+        </a>
+        {this.state.isOpen && <Comments comments={this.props.comments} />}
+      </div>
+    );
   }
 }
 
--- app/shared/comments/CommentsContainer.spec.js
+++ app/shared/comments/CommentsContainer.spec.js
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import React from 'react';
+import FontAwesome from 'react-fontawesome';
 import simple from 'simple-mock';
 
 import { fetchComments } from 'api/actions';
@@ -12,6 +13,7 @@ function getWrapper(props) {
   const defaults = {
     comments: [],
     fetchComments: () => null,
+    name: 'Varauksen viestit',
     reservationId: 1,
   };
   return shallow(<CommentsContainer {...defaults} {...props} />);
@@ -33,12 +35,117 @@ describe('shared/comments/CommentsContainer', () => {
       });
     });
 
+    it('has correct initial state', () => {
+      const wrapper = getWrapper();
+      expect(wrapper.state()).to.deep.equal({ isOpen: false });
+    });
+
+    describe('toggleComments', () => {
+      function toggleComments({ isOpen = false, preventDefault = (() => null), ...props }) {
+        const wrapper = getWrapper(props);
+        if (isOpen) wrapper.setState({ isOpen });
+        wrapper.instance().toggleComments({ preventDefault });
+        return wrapper;
+      }
+
+      it('calls event.preventDefault', () => {
+        const preventDefault = simple.mock();
+        toggleComments({ preventDefault });
+        expect(preventDefault.callCount).to.equal(1);
+        expect(preventDefault.lastCall.args).to.deep.equal([]);
+      });
+
+      it('sets isOpen from false to true if comments exist', () => {
+        const comments = [{ id: 1, content: '', createdAt: '', user: { name: '' } }];
+        const wrapper = toggleComments({ isOpen: false, comments });
+        expect(wrapper.state('isOpen')).to.be.true;
+      });
+
+      it('does not set isOpen from false to true if comments is []', () => {
+        const comments = [];
+        const wrapper = toggleComments({ isOpen: false, comments });
+        expect(wrapper.state('isOpen')).to.be.false;
+      });
+
+      it('does not set isOpen from false to true if comments is null', () => {
+        const comments = null;
+        const wrapper = toggleComments({ isOpen: false, comments });
+        expect(wrapper.state('isOpen')).to.be.false;
+      });
+
+      it('sets isOpen from true to false', () => {
+        const wrapper = toggleComments({ isOpen: true });
+        expect(wrapper.state('isOpen')).to.be.false;
+      });
+    });
+
     describe('render', () => {
-      it('renders Comments with comments', () => {
-        const comments = [{ id: 1, content: 'Comment', createdAt: '', user: { name: 'U' } }];
-        const comment = getWrapper({ comments }).find(Comments);
-        expect(comment).to.have.length(1);
-        expect(comment.prop('comments')).to.equal(comments);
+      describe('toggle link', () => {
+        function getLink({ comments = [], name = 'Name', isOpen = false } = {}) {
+          const wrapper = getWrapper({ comments, name });
+          if (isOpen) wrapper.setState({ isOpen });
+          return wrapper.find('a');
+        }
+
+        it('renders toggle link', () => {
+          const link = getLink();
+          expect(link).to.have.length(1);
+        });
+
+        it('renders label when comments is null', () => {
+          const link = getLink({ comments: null, name: 'Testin viestit' });
+          expect(link.text()).to.equal('Testin viestit (-) <FontAwesome />');
+        });
+
+        it('renders label when comments is empty array', () => {
+          const link = getLink({ comments: [], name: 'Testin viestit' });
+          expect(link.text()).to.equal('Testin viestit (0) <FontAwesome />');
+        });
+
+        it('renders label when comments exist', () => {
+          const comments = [{ id: 1, content: '', createdAt: '', user: { name: '' } }];
+          const link = getLink({ comments, name: 'Testin viestit' });
+          expect(link.text()).to.equal('Testin viestit (1) <FontAwesome />');
+        });
+
+        it('renders caret-down when open', () => {
+          const icon = getLink({ isOpen: true }).find(FontAwesome);
+          expect(icon).to.have.length(1);
+          expect(icon.prop('name')).to.equal('caret-down');
+        });
+
+        it('renders caret-right when closed', () => {
+          const icon = getLink({ isOpen: false }).find(FontAwesome);
+          expect(icon).to.have.length(1);
+          expect(icon.prop('name')).to.equal('caret-right');
+        });
+      });
+
+      describe('Comments', () => {
+        it('is not rendered if no comments', () => {
+          const comments = null;
+          const wrapper = getWrapper({ comments });
+          expect(wrapper.state('isOpen')).to.be.false;
+          const comment = wrapper.find(Comments);
+          expect(comment).to.have.length(0);
+        });
+
+        it('is not rendered when closed', () => {
+          const comments = [{ id: 1, content: 'Comment', createdAt: '', user: { name: 'U' } }];
+          const wrapper = getWrapper({ comments });
+          expect(wrapper.state('isOpen')).to.be.false;
+          const comment = wrapper.find(Comments);
+          expect(comment).to.have.length(0);
+        });
+
+        it('is rendered when open', () => {
+          const comments = [{ id: 1, content: 'Comment', createdAt: '', user: { name: 'U' } }];
+          const wrapper = getWrapper({ comments });
+          wrapper.setState({ isOpen: true });
+          const comment = wrapper.find(Comments);
+          expect(comment).to.have.length(1);
+          expect(comment.prop('comments')).to.equal(comments);
+        });
       });
     });
   });
