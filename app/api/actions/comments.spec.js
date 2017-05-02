16fb4d8490c2f631e80a4faeb8484089967347c7
Oskari Hiltunen <felix.loether@gmail.com>
2017-05-02 12:17:23 +0300

    Support cateringId in fetchComments action
    Refs #154.

--- app/api/actions/comments.spec.js
+++ app/api/actions/comments.spec.js
@@ -4,7 +4,9 @@ import { createComment, fetchComments } from './comments';
 
 describe('api/actions/comments', () => {
   describe('fetchComments', () => {
+    const cateringId = 85559;
     const reservationId = 3819;
+
     createApiTest({
       name: 'fetchComments',
       action: fetchComments,
@@ -12,6 +14,25 @@ describe('api/actions/comments', () => {
       tests: {
         method: 'GET',
         endpoint: `http://www.mocky.io/v2/58ffa5c7110000ef16f60030?reservation_id=${reservationId}`,
+        request: {
+          type: types.COMMENTS_GET_REQUEST,
+        },
+        success: {
+          type: types.COMMENTS_GET_SUCCESS,
+        },
+        error: {
+          type: types.COMMENTS_GET_ERROR,
+        },
+      },
+    });
+
+    createApiTest({
+      name: 'fetchComments',
+      action: fetchComments,
+      args: [{ cateringId }],
+      tests: {
+        method: 'GET',
+        endpoint: `http://www.mocky.io/v2/58ffa5c7110000ef16f60030?catering_id=${cateringId}`,
         request: {
           type: types.COMMENTS_GET_REQUEST,
         },
