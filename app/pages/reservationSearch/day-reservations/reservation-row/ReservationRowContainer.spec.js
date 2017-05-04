3fe296c2a87757aaf51c90075c6d7c72a6a02426
Eero Vehmanen <eero.vehmanen@gmail.com>
2017-05-04 16:11:52 +0300

    Show number of participants in ReservationRow (refs #165)

--- app/pages/reservationSearch/day-reservations/reservation-row/ReservationRowContainer.spec.js
+++ app/pages/reservationSearch/day-reservations/reservation-row/ReservationRowContainer.spec.js
@@ -13,6 +13,7 @@ describe('pages/reservationSearch/day-reservations/reservation-row/ReservationRo
     eventSubject: 'Meeting',
     hostName: 'Michael Jackson',
     id: 123,
+    numberOfParticipants: 15,
     onClick: () => null,
     timeRange: '18:00 - 19:30',
   };
@@ -26,6 +27,7 @@ describe('pages/reservationSearch/day-reservations/reservation-row/ReservationRo
     expect(row.is(ReservationRow)).to.be.true;
     expect(row.prop('eventSubject')).to.equal(defaults.eventSubject);
     expect(row.prop('hostName')).to.equal(defaults.hostName);
+    expect(row.prop('numberOfParticipants')).to.equal(defaults.numberOfParticipants);
     expect(row.prop('onClick')).to.be.a('function');
     expect(row.prop('place')).to.equal(defaults.place);
     expect(row.prop('timeRange')).to.equal(defaults.timeRange);
