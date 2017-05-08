4761804dbf408e491ec2b952cb164d18a215fcec
Eero Vehmanen <eero.vehmanen@gmail.com>
2017-05-08 10:03:55 +0300

    Fix timezone issue in tests (refs #171)
    "time is correct" test in ReservationEditFormContainer.spec.js failed in Circle. When expected time was 10:00, actual time was 07:00. This happened probably because setting timezone to Helsinki was done only in application but not in tests.

--- config/specs.bootstrap.js
+++ config/specs.bootstrap.js
@@ -1,10 +1,14 @@
 import 'babel-polyfill';
 import 'moment/locale/fi';
+import 'moment-timezone';
+import moment from 'moment';
 
 /* eslint-disable no-console */
 console.warning = (...args) => { throw Error(`console.warning: ${args.join(' ')}`); };
 console.error = (...args) => { throw Error(`console.error: ${args.join(' ')}`); };
 /* eslint-enable */
+
+moment.tz.setDefault('Europe/Helsinki');
 
 const testsContext = require.context('../app', true, /spec.js$/);
 testsContext.keys().forEach(testsContext);
