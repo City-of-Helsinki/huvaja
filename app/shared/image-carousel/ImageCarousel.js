ae8c006e9e719b9ab9161c80fe275597c3846c92
Raúl García <serulo@gmail.com>
2017-05-10 17:28:40 +0200

    Preload images on carousel
    Refs #128.

--- app/shared/image-carousel/ImageCarousel.js
+++ app/shared/image-carousel/ImageCarousel.js
@@ -13,16 +13,32 @@ function renderCarouselItem(image) {
   );
 }
 
+function preloadImage(image) {
+  // Renders an empty div with the background image. This div won't be shown as it doesn't have
+  // content but it will load the background-image. Otherwise Carousel background images won't
+  // be loaded until "Carousel.Item" display css attribute is changed to something different than
+  // "none". Some browsers are smart enough to avoid loading not diplayed content.
+  return (
+    <div
+      key={image.url}
+      style={{ backgroundImage: `url(${image.url}` }}
+    />
+  );
+}
+
 function ImageCarousel({ images }) {
   return (
-    <Carousel
-      className="image-carousel"
-      controls={images.length > 1}
-      indicators={images.length > 1}
-      interval={carouselInterval}
-    >
-      {images.map(renderCarouselItem)}
-    </Carousel>
+    <div>
+      {images.map(preloadImage)}
+      <Carousel
+        className="image-carousel"
+        controls={images.length > 1}
+        indicators={images.length > 1}
+        interval={carouselInterval}
+      >
+        {images.map(renderCarouselItem)}
+      </Carousel>
+    </div>
   );
 }
 
