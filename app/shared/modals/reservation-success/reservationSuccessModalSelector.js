e21cb4e82d76f49696918faf7e326cc959773f47
Eero Vehmanen <eero.vehmanen@gmail.com>
2017-05-28 20:05:50 +0300

    Show created and failed reservations in modal (refs #216)

---
+++ app/shared/modals/reservation-success/reservationSuccessModalSelector.js
@@ -0,0 +1,40 @@
+import mapValues from 'lodash/mapValues';
+import orderBy from 'lodash/orderBy';
+import { createSelector, createStructuredSelector } from 'reselect';
+
+const showSelector = state => state.modals.reservationSuccess.show;
+const createdSelector = state => orderBy(
+  state.modals.reservationSuccess.created,
+  'begin'
+);
+const failedReservationsSelector = state => orderBy(
+  state.modals.reservationSuccess.failed,
+  'begin'
+);
+
+function resourcesSelector(state) {
+  return state.data.resources;
+}
+
+function unitsSelector(state) {
+  return state.data.units;
+}
+
+const resourceNamesSelector = createSelector(
+  resourcesSelector,
+  unitsSelector,
+  (resources, units) => mapValues(resources, (resource) => {
+    const unit = units[resource.unit];
+    const unitName = unit && unit.name.fi;
+    return unitName ?
+      `${unitName} / ${resource.name.fi}` :
+      null;
+  })
+);
+
+export default createStructuredSelector({
+  createdReservations: createdSelector,
+  failedReservations: failedReservationsSelector,
+  resourceNames: resourceNamesSelector,
+  show: showSelector,
+});
