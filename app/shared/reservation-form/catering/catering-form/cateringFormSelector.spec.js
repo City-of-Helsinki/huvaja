bc630cff05df56907d9d813fc8992f169e3ab48e
Oskari Hiltunen <git@loethr.net>
2017-05-29 12:47:06 +0300

    Use priceListUrl if available
    Refs #231.

--- app/shared/reservation-form/catering/catering-form/cateringFormSelector.spec.js
+++ app/shared/reservation-form/catering/catering-form/cateringFormSelector.spec.js
@@ -40,6 +40,20 @@ describe('shared/reservation-form/catering/catering-form/cateringFormSelector', 
     expect(selected.cateringMenuItems[2]).to.deep.equal(cateringMenuItems.products[2]);
   });
 
+  it('returns cateringProvider from state', () => {
+    const state = {
+      'form.resourceReservation.values': {
+        resource: 'r1',
+        time: { begin: {}, end: {} },
+      },
+      'data.resources': { r1: { id: 'r1', unit: 'u1' } },
+      'data.units': { u1: { id: 'u1' } },
+      'data.cateringProviders': { c1: { id: 'c1', units: ['u1'], priceListUrl: 'example' } },
+    };
+    const selected = getSelected(state);
+    expect(selected.cateringProvider).to.deep.equal(state['data.cateringProviders'].c1);
+  });
+
   describe('defaultCateringTime', () => {
     it('returns reservation begin time if it is set', () => {
       const time = {
