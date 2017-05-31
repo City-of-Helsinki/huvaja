2ff480b7782d49c56f04a24d3f8fd6768662e131
Eero Vehmanen <eero.vehmanen@gmail.com>
2017-05-31 14:29:54 +0300

    Test CompactReservationList (refs #216)

---
+++ app/utils/fixtures/Resource.js
@@ -0,0 +1,8 @@
+import { Factory } from 'rosie';
+
+const Resource = new Factory()
+  .sequence('id', index => `r-${index}`)
+  .sequence('name', index => { fi: `Resource-${index}` })
+  .sequence('unit', index => `u-${index}`);
+
+export default Resource;
