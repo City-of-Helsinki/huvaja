4689b689dece2450f7e0e02b316606cad14d4c7b
Eero Vehmanen <eero.vehmanen@gmail.com>
2017-05-31 13:37:48 +0300

    Test timeUtils (refs #216)

--- app/utils/timeUtils.spec.js
+++ app/utils/timeUtils.spec.js
@@ -18,4 +18,53 @@ describe('utils/timeUtils', () => {
       expect(actual).to.be.null;
     });
   });
+
+  describe('getDateTimeRangeFieldMoments', () => {
+    const defaultValue = {
+      begin: {
+        date: '2017-02-02',
+        time: '10:00',
+      },
+      end: {
+        date: '2017-02-02',
+        time: '12:00',
+      },
+    };
+
+    it('returns moments for begin and end', () => {
+      const actual = timeUtils.getDateTimeRangeFieldMoments(defaultValue);
+      const expected = {
+        begin: moment('2017-02-02 10:00'),
+        end: moment('2017-02-02 12:00'),
+      };
+      expect(expected.begin.isSame(actual.begin)).to.be.true;
+      expect(expected.end.isSame(actual.end)).to.be.true;
+    });
+
+    it('returns null for begin when not valid date time', () => {
+      const value = {
+        ...defaultValue,
+        begin: {
+          date: '2017-02-02',
+          time: '10:0-',
+        },
+      };
+      const actual = timeUtils.getDateTimeRangeFieldMoments(value);
+      expect(actual.begin).to.be.null;
+      expect(actual.end).to.not.be.null;
+    });
+
+    it('returns null for end when not valid date time', () => {
+      const value = {
+        ...defaultValue,
+        end: {
+          date: '2017-02-02',
+          time: '',
+        },
+      };
+      const actual = timeUtils.getDateTimeRangeFieldMoments(value);
+      expect(actual.end).to.be.null;
+      expect(actual.begin).to.not.be.null;
+    });
+  });
 });
