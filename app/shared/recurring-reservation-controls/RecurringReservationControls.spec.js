e9fd8dbe406e290765001a4c92e6044fb7a644d6
Eero Vehmanen <eero.vehmanen@gmail.com>
2017-05-31 14:41:29 +0300

    Test RecurringReservationControls (refs #216)

---
+++ app/shared/recurring-reservation-controls/RecurringReservationControls.spec.js
@@ -0,0 +1,86 @@
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import React from 'react';
+import NumericInput from 'react-numeric-input';
+import Select from 'react-select';
+import simple from 'simple-mock';
+
+import DatePicker from 'shared/date-picker';
+import RecurringReservationControls from './RecurringReservationControls';
+
+function getWrapper(props) {
+  const defaults = {
+    changeFrequency: () => null,
+    changeLastTime: () => null,
+    changeNumberOfOccurrences: () => null,
+    frequency: 'days',
+    frequencyOptions: [{ label: '', value: '' }],
+    isVisible: true,
+    lastTime: null,
+    numberOfOccurrences: 1,
+  };
+  return shallow(<RecurringReservationControls {...defaults} {...props} />);
+}
+
+describe('shared/recurring-reservation-controls/RecurringReservationControls', () => {
+  it('renders an empty span if isVisible is false', () => {
+    const wrapper = getWrapper({ isVisible: false });
+    expect(wrapper.equals(<span />)).to.be.true;
+  });
+
+  it('renders Select with correct props', () => {
+    const props = {
+      changeFrequency: () => null,
+      frequency: 'days',
+      frequencyOptions: [{ label: '', value: '' }],
+      lastTime: '2017-04-09',
+    };
+    const select = getWrapper(props).find(Select);
+    expect(select).to.have.length(1);
+    expect(select.prop('onChange')).to.equal(props.changeFrequency);
+    expect(select.prop('options')).to.equal(props.frequencyOptions);
+    expect(select.prop('value')).to.equal(props.frequency);
+  });
+
+  it('renders NumericInput to change number of occurrences', () => {
+    const props = {
+      changeNumberOfOccurrences: () => null,
+      lastTime: '2017-04-09',
+      numberOfOccurrences: 12,
+    };
+    const control = getWrapper(props).find(NumericInput);
+    expect(control).to.have.length(1);
+    expect(control.prop('min')).to.equal(1);
+    expect(control.prop('value')).to.equal(props.numberOfOccurrences);
+    expect(control.prop('onChange')).to.equal(props.changeNumberOfOccurrences);
+  });
+
+  it('renders DatePicker with correct props', () => {
+    const props = {
+      lastTime: '2017-04-09',
+      numberOfOccurrences: 12,
+      changeLastTime: simple.mock(),
+    };
+    const control = getWrapper(props).find(DatePicker);
+    expect(control).to.have.length(1);
+    expect(control.prop('dateFormat')).to.equal('D.M.YYYY');
+    expect(control.prop('formControl')).to.be.true;
+    expect(control.prop('onChange')).to.equal(props.changeLastTime);
+    expect(control.prop('value')).to.equal(props.lastTime);
+  });
+
+  describe('without set frecuency', () => {
+    let wrapper;
+    before(() => {
+      wrapper = getWrapper({ frequency: '' });
+    });
+
+    it('does not render numbrerOfOcurrences NumericInput', () => {
+      expect(wrapper.find(NumericInput)).to.have.length(0);
+    });
+
+    it('does not render DatePicker', () => {
+      expect(wrapper.find(DatePicker)).to.have.length(0);
+    });
+  });
+});
---
+++ app/shared/recurring-reservation-controls/RecurringReservationControlsContainer.spec.js
@@ -0,0 +1,59 @@
+import { expect } from 'chai';
+
+import { getState } from 'utils/testUtils';
+import { selector } from './RecurringReservationControlsContainer';
+
+describe('shared/recurring-reservation-controls/RecurringReservationControlsContainer', () => {
+  describe('selector', () => {
+    function getSelected(state) {
+      return selector(state);
+    }
+
+    it('returns frequency from state', () => {
+      const frequency = 'days';
+      const state = getState({
+        recurringReservations: { frequency },
+      });
+      expect(getSelected(state).frequency).to.equal(frequency);
+    });
+
+    it('returns lastTime from state', () => {
+      const lastTime = '2017-04-18';
+      const state = getState({
+        recurringReservations: { lastTime },
+      });
+      expect(getSelected(state).lastTime).to.equal(lastTime);
+    });
+
+    it('returns frequencyOptions', () => {
+      const state = getState();
+      expect(getSelected(state).frequencyOptions).to.exist;
+    });
+
+    describe('isVisible', () => {
+      it('returns true if baseTime is set', () => {
+        const baseTime = { begin: 'mock-begin', end: 'mock-end' };
+        const state = getState({
+          recurringReservations: { baseTime },
+        });
+        expect(getSelected(state).isVisible).to.be.true;
+      });
+
+      it('returns false if baseTime is not set', () => {
+        const baseTime = null;
+        const state = getState({
+          recurringReservations: { baseTime },
+        });
+        expect(getSelected(state).isVisible).to.be.false;
+      });
+    });
+
+    it('returns numberOfOccurrences from state', () => {
+      const numberOfOccurrences = 12;
+      const state = getState({
+        recurringReservations: { numberOfOccurrences },
+      });
+      expect(getSelected(state).numberOfOccurrences).to.equal(numberOfOccurrences);
+    });
+  });
+});
