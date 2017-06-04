d43867969d325edcd7a0b806eaeb3b7cddf0ce6f
Eero Vehmanen <eero.vehmanen@gmail.com>
2017-06-04 17:02:15 +0300

    POST catering order after POSTing reservation (refs #242)
    Error handling has not been implemented well yet. If creating catering order fails, alert message "Tarjoilutilauksen tekeminen epäonnistui." is shown in reservation form. It's not clear to user that creating reservation succeeded. This can be improved later.

--- app/api/utils.js
+++ app/api/utils.js
@@ -1,4 +1,33 @@
+import { SubmissionError } from 'redux-form';
+
 import actionTypes from './actionTypes';
+
+// Proper error handling has not been implemented yet.
+function getErrors(action) {
+  if (action.type === 'CATERING_ORDER_POST_ERROR') {
+    return { _error: 'Tarjoilutilauksen tekeminen epäonnistui.' };
+  }
+  return { _error: 'Jokin meni pieleen.' };
+}
+
+function createAPIPromise(callback) {
+  return function handleAPIAction(apiAction) {
+    let resolve;
+    let reject;
+    const promise = new Promise((res, rej) => {
+      resolve = res;
+      reject = rej;
+    }).catch((action) => {
+      throw new SubmissionError(getErrors(action));
+    });
+    const actionOptions = {
+      errorMeta: { sideEffect: reject },
+      successMeta: { sideEffect: resolve },
+    };
+    callback(actionOptions, apiAction);
+    return promise;
+  };
+}
 
 function getApiActionName(actionType) {
   return actionType.split('_').slice(0, -1).join('_');
@@ -9,6 +38,7 @@ function isApiAction(actionType) {
 }
 
 export {
+  createAPIPromise,
   getApiActionName,
   isApiAction,
 };
