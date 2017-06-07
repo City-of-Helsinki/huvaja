088e154ff00e2261142a10ff16bf6f464d77ca4a
Eero Vehmanen <eero.vehmanen@gmail.com>
2017-06-07 22:35:41 +0300

    Save mapping: reservation id -> catering order id (refs #243)

---
+++ app/state/reducers/reservationCateringOrdersReducer.js
@@ -0,0 +1,17 @@
+import fromPairs from 'lodash/fromPairs';
+import { handleActions } from 'redux-actions';
+
+import actionTypes from 'api/actionTypes';
+
+// State will map reservation ids to catering order ids.
+const initialState = {};
+
+export default handleActions({
+  [actionTypes.CATERING_ORDER_GET_SUCCESS]: (state, action) => {
+    if (!action.payload.entities.cateringOrders) return state;
+    const orders = Object.values(action.payload.entities.cateringOrders);
+    const pairs = orders.map(order => [order.reservation, order.id]);
+    const newData = fromPairs(pairs);
+    return { ...state, ...newData };
+  },
+}, initialState);
