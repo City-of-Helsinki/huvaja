1440e5b663da977d3de0ca3e912a9072f26b7dee
Eero Vehmanen <eero.vehmanen@gmail.com>
2017-06-07 22:26:01 +0300

    Customize CateringOrderTable (refs #243)
    - Allows hiding header
    - Allows merging name and description columns

--- app/shared/reservation-form/catering/CateringOrderTable.js
+++ app/shared/reservation-form/catering/CateringOrderTable.js
@@ -6,24 +6,38 @@ import FontAwesome from 'react-fontawesome';
 CateringOrderTable.propTypes = {
   editOrder: PropTypes.func,
   items: PropTypes.array.isRequired,
+  narrow: PropTypes.bool,
+  noHeader: PropTypes.bool,
 };
-
 function CateringOrderTable(props) {
   return (
     <Table className="catering-order-table" striped>
-      <thead>
-        <tr>
-          <th className="product">Tuote</th>
-          <th className="description">Kuvaus</th>
-          <th className="number">Määrä</th>
-          {props.editOrder && <th />}
-        </tr>
-      </thead>
+      {!props.noHeader &&
+        <thead>
+          <tr>
+            <th className="product">Tuote</th>
+            {!props.narrow && <th className="description">Kuvaus</th>}
+            <th className="number">Määrä</th>
+            {props.editOrder && <th />}
+          </tr>
+        </thead>
+      }
       <tbody>
         {props.items.map(item =>
           <tr key={item.id}>
-            <td className="product">{item.name.fi}</td>
-            <td className="description">{item.description && item.description.fi}</td>
+            <td className="product">
+              <div>{item.name.fi}</div>
+              {props.narrow &&
+                <div className="catering-order-table-narrow-description">
+                  {item.description && item.description.fi}
+                </div>
+              }
+            </td>
+            {!props.narrow &&
+              <td className="description">
+                {item.description && item.description.fi}
+              </td>
+            }
             <td className="number">
               {props.editOrder &&
                 <FormControl
