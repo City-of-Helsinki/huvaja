0275f70d66a721c93ff1a4d71ccb57b29f2be4a4
Eero Vehmanen <eero.vehmanen@gmail.com>
2017-06-13 20:13:14 +0300

    Add more catering util functions (refs #211)

--- app/utils/cateringUtils.js
+++ app/utils/cateringUtils.js
@@ -1,3 +1,4 @@
+import fromPairs from 'lodash/fromPairs';
 import moment from 'moment';
 
 function formatServingTime(time) {
@@ -10,7 +11,39 @@ function getServingTimeText(servingTime) {
     'Varauksen alkamisaika';
 }
 
+// [{ product: 2, quantity: 10 }] -> { 2: 10 }
+function orderLinesToOrderObject(orderLines) {
+  const pairs = orderLines.map(line => [line.product, line.quantity]);
+  return fromPairs(pairs);
+}
+
+// { 2: 10 } -> [{ product: 2, quantity: 10 }]
+function orderObjectToOrderLines(order) {
+  return Object.keys(order).map(productId => ({
+    product: parseInt(productId, 10),
+    quantity: order[productId],
+  }));
+}
+
+function cateringOrderToFormValue(cateringOrder) {
+  const { orderLines, ...rest } = cateringOrder;
+  return {
+    ...rest,
+    order: orderLinesToOrderObject(orderLines),
+  };
+}
+
+function formValueToCateringOrder(value) {
+  const { order, ...rest } = value;
+  return {
+    ...rest,
+    orderLines: orderObjectToOrderLines(order),
+  };
+}
+
 export default {
+  cateringOrderToFormValue,
   formatServingTime,
+  formValueToCateringOrder,
   getServingTimeText,
 };
--- app/utils/cateringUtils.spec.js
+++ app/utils/cateringUtils.spec.js
@@ -3,6 +3,19 @@ import { expect } from 'chai';
 import cateringUtils from 'utils/cateringUtils';
 
 describe('utils/cateringUtils', () => {
+  const cateringOrder = {
+    invoincingData: '123',
+    orderLines: [
+      { product: 3, quantity: 10 },
+    ],
+  };
+  const formValue = {
+    invoincingData: '123',
+    order: {
+      3: 10,
+    },
+  };
+
   describe('formatServingTime', () => {
     it('returns time in correct format', () => {
       const actual = cateringUtils.formatServingTime('09:30:00');
@@ -19,6 +32,20 @@ describe('utils/cateringUtils', () => {
     it('returns text when serving time is null', () => {
       const actual = cateringUtils.getServingTimeText(null);
       expect(actual).to.equal('Varauksen alkamisaika');
+    });
+  });
+
+  describe('cateringOrderToFormValue', () => {
+    it('changes orderLines to order object', () => {
+      const actual = cateringUtils.cateringOrderToFormValue(cateringOrder);
+      expect(actual).to.deep.equal(formValue);
+    });
+  });
+
+  describe('formValueToCateringOrder', () => {
+    it('changes orderLines to order object', () => {
+      const actual = cateringUtils.formValueToCateringOrder(formValue);
+      expect(actual).to.deep.equal(cateringOrder);
     });
   });
 });
