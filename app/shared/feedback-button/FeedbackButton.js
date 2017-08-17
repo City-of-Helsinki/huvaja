27c41a230e1f914824be8a70de1d0d59690b465d
Juha Yrjölä <juha.yrjola@iki.fi>
2017-08-17 12:59:16 +0300

    Change feedback mailto URL subject

--- app/shared/feedback-button/FeedbackButton.js
+++ app/shared/feedback-button/FeedbackButton.js
@@ -3,7 +3,7 @@ import React from 'react';
 function FeedbackButton() {
   return (
     <div className="feedback-floating">
-      <a href="mailto:dev@hel.fi?subject=Huonevaraus feedback" className="feedback-trigger--float">
+      <a href="mailto:dev@hel.fi?subject=Huonevaraus-palaute" className="feedback-trigger--float">
         <div className="btn btn-beta">Beta</div>
         <div className="btn btn-primary">
           Anna palautetta
