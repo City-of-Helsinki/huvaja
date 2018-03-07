10163b5474e192733526be6a632ae7d41af49f66
Juha Yrjölä <juha.yrjola@iki.fi>
2018-03-07 09:41:41 +0200

    Remove beta label

--- app/shared/feedback-button/FeedbackButton.js
+++ app/shared/feedback-button/FeedbackButton.js
@@ -4,7 +4,6 @@ function FeedbackButton() {
   return (
     <div className="feedback-floating">
       <a href="mailto:dev@hel.fi?subject=Huonevaraus-palaute" className="feedback-trigger--float">
-        <div className="btn btn-beta">Beta</div>
         <div className="btn btn-primary">
           Anna palautetta
         </div>
